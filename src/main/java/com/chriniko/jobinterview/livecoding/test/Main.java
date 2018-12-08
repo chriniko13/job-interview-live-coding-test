11c4690e4d7484f7dec2e39b35ef3980c70fa468
Nikolaos Christidis <nick.christidis@glispamedia.com>
2018-12-08 23:31:37 +0200

    minor changes

--- src/main/java/com/chriniko/jobinterview/livecoding/test/Main.java
+++ src/main/java/com/chriniko/jobinterview/livecoding/test/Main.java
@@ -1,13 +1,28 @@
 package com.chriniko.jobinterview.livecoding.test;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        //new FirstExercise().run();
+        if (args.length != 1
+                || !Arrays.asList("1", "2", "3").contains(args[0])) {
+            System.out.println("Please enter number of exercise to run, selections: 1 or 2 or 3");
+            System.exit(-1);
+        }
 
-        new SecondExercise().run();
+        switch (args[0]){
+            case "1":
+                new FirstExercise().run();
+                break;
+            case "2":
+                new SecondExercise().run();
+                break;
+            case "3":
+                new ThirdExercise().run();
+                break;
+        }
 
-        //new ThirdExercise().run();
     }
 }
